a104e40a5a33a0f7e382fe6cbee11276e54f05bf
alice <alice.martinez.28@gmail.com>
2018-11-13 13:24:35 +0100

    Change drawables name

--- app/src/main/java/com/openclassroom/alice/moodtracker/Controller/MainActivity.java
+++ app/src/main/java/com/openclassroom/alice/moodtracker/Controller/MainActivity.java
@@ -1,7 +1,9 @@
-package com.openclassroom.alice.moodtracker;
+package com.openclassroom.alice.moodtracker.Controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.openclassroom.alice.moodtracker.R;
 
 public class MainActivity extends AppCompatActivity {
 
